a5d8c618ca48148f80e3a3a4f898e3ff3547d7f9
fanglemeng <fanglemeng@163.com>
2019-10-23 16:32:29 +0800

    login完成

---
+++ public/back/js/common.js
@@ -0,0 +1,12 @@
+/*开起进度条*/
+NProgress.start();
+
+/*结束进度条*/
+NProgress.done();
+
+$(document).ajaxStart(function(){
+  NProgress.start();
+});
+$(document).ajaxStop(function(){
+  NProgress.done();
+});
--- public/back/js/login.js
+++ public/back/js/login.js
@@ -16,6 +16,9 @@ $(function () {
             min: 2,
             max: 6,
             message: "用户名长度必须在2-6位"
+          },
+          callback:{
+            message:"用户名不存在"
           }
         }
       },
@@ -28,9 +31,39 @@ $(function () {
             min: 6,
             max: 12,
             message: "密码长度必须是6-12位"
+          },
+          callback:{
+            message:"密码错误"
           }
         }
       }
     }
+  });
+  $("#form").on("success.form.bv",function(e){
+    e.preventDefault();
+    console.log("检验成功，默认行为被阻止");
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      data:$("#form").serialize(),
+      dataType:"json",
+      success:function(info){
+        // console.log(info);
+        if(info.success){
+          location.href="index.html";
+        }
+        if(info.error===1000){
+          
+          $("#form").data("bootstrapValidator").updateStatus("username","INVALID","callback");
+        }
+        if(info.error===1001){
+          $("#form").data("bootstrapValidator").updateStatus("password","INVALID","callback");
+        }
+      }
+    });
+  });
+  $("[type=reset]").click(function(){
+    // console.log("cccccc");
+    $("#form").data("bootstrapValidator").resetForm();
   });
 });
